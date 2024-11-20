26c877cbb0c359a20bca64069f68fd7da529141a
justice0317 <152950658+justice0317@users.noreply.github.com>
2024-11-20 10:00:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function redirectTo(action) {
     if (action === 'login') {
         window.location.href = '你的登入網址'; // 替換成你的登入頁面網址
     } else if (action === 'signup') {
-        window.location.href = '你的註冊網址'; // 替換成你的註冊頁面網址
+        window.location.href = 'https://signin-black.vercel.app'; // 替換成你的註冊頁面網址
     }
 }
