2c6262660a79a5ac1978fd5cd8baf14b0a4cb8a7
justice0317 <152950658+justice0317@users.noreply.github.com>
2024-11-20 10:23:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 按鈕點擊導向
 function redirectTo(action) {
     if (action === 'login') {
-        window.location.href = '你的登入網址'; // 替換成你的登入頁面網址
+        window.location.href = 'https://login-phi-amber.vercel.app'; // 替換成你的登入頁面網址
     } else if (action === 'signup') {
         window.location.href = 'https://signin-black.vercel.app'; // 替換成你的註冊頁面網址
     }
