fb164b974293a1d754f356c178ca96e1037d206a
justice0317 <152950658+justice0317@users.noreply.github.com>
2024-12-14 17:14:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// 按鈕點擊導向
-function redirectTo(action) {
-    if (action === 'login') {
-        window.location.href = 'https://login-phi-amber.vercel.app'; // 替換成你的登入頁面網址
-    } else if (action === 'signup') {
-        window.location.href = 'https://signin-black.vercel.app'; // 替換成你的註冊頁面網址
-    }
+function redirectTo(page) {
+    window.location.href = page;
 }
